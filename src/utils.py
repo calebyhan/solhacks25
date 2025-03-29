424114930a825f47b806504899cf182c05c7f156
calebyhan <calebhantech@gmail.com>
2025-03-29 11:31:07 -0400

    commit

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from multipledispatch import dispatch
 from datetime import datetime
+import json
 
 @dispatch()
 def dates() -> str:
@@ -37,6 +38,43 @@ def dates(date: datetime) -> str:
     """
     return date.strftime("%Y-%m-%d %H:%M:%S")
 
+def report(reportdate: str, location: float[2], details: str, tactics: str, verification: int, status: str) -> dict:
+    """
+    Generates a report dictionary.
+
+    Args:
+        reportdate (str): The date of the report.
+        details (str): The details of the report.
+        tactics (str): The tactics used in the report.
+        verification (int): The verification status.
+        status (str): The status of the report.
+
+    Returns:
+        dict: The generated report dictionary.
+    """
+    return {
+        "date": dates(),
+        "report_date": dates(reportdate),
+        "location": "{}, {}".format(location[0], location[1]),
+        "details": details,
+        "tactics": tactics,
+        "verification": verification,
+        "status": status
+    }
+
+def add_data(data: dict) -> None:
+    """
+    Adds data to the JSON file.
+
+    Args:
+        data (dict): The data to add.
+    """
+    with open('data.json', 'a') as f:
+        d = json.load(f)
+        d["n"] += 1
+        d["last_update"] = dates()
+        d["data"].append(data)
+
 def get_data(i: int, startDate: str, endDate: str) -> str:
     """
     Returns all api data in JSON format.
@@ -45,5 +83,13 @@ def get_data(i: int, startDate: str, endDate: str) -> str:
         str: The JSON data.
     """
     with open('data.json', 'r') as f:
-        data = f.read()
+        data = json.load(f)
+    if startDate:
+        startDate = dates(startDate)
+    if endDate:
+        endDate = dates(endDate)
+    if startDate and endDate:
+        data["data"] = [d for d in data["data"] if startDate <= dates(d["timestamp"]) <= endDate]
+    if i:
+        data["data"] = data["data"][-i:]
     return data
