ccd3e53321277848176e001406754085d5144d70
NikolaosJPhil <nikolaos@philippakos.com>
2025-03-30 09:25:51 -0400

    finished resources, starting about and rights

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,17 +1,24 @@
 
 
 //for the resources sidebar
-function toggleSubMenu(categoryId) {
-    console.log("Toggling submenu for: " + categoryId); 
+function toggleSubMenu(categoryId) { 
      // Get the submenu based on the provided category ID
      var submenu = document.getElementById(categoryId);
     
      // Toggle the submenu display between 'none' (hidden) and 'block' (visible)
      if (submenu.style.display === "none" || submenu.style.display === "") {
+        const allSubmenus = document.querySelectorAll('.submenu');
+            allSubmenus.forEach(sub => {
+                if (sub !== submenu) {
+                    sub.style.display = 'none';  // Hide other submenus
+                }
+            });
+        
          submenu.style.display = "block";  // Show the submenu
      } else {
          submenu.style.display = "none";   // Hide the submenu
-     }
+    }
+    
 }
 
 //GPT is god
